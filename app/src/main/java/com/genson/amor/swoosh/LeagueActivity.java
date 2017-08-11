058e0afe8cff64e7ad2a3bd94ed7d83a9fca1c70
amor genson <maleahamorgenson@gmail.com>
2017-08-11 19:06:10 +0800

    Unfinished Activities Commit

--- app/src/main/java/com/genson/amor/swoosh/LeagueActivity.java
+++ app/src/main/java/com/genson/amor/swoosh/LeagueActivity.java
@@ -5,10 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 
 public class LeagueActivity extends AppCompatActivity {
 
+    private Button mBtnNext;
+    private Button mBtnNextBG;
+    private Button mBtnMens;
+    private Button mBtnWomens;
+    private Button mBtnCoed;
+    private TextView mtvIAmText;
+    private TextView mtvDSkill;
+    public static final String PASS_STRING = "";
+    public static final int REQ_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,27 +25,103 @@ public class LeagueActivity extends AppCompatActivity {
         setContentView(R.layout.activity_league);
         findViews();
 
+        mBtnMens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mBtnMens.getAlpha() != mBtnNext.getAlpha()) {
+                    mBtnMens.setAlpha(1f);
+                    mBtnWomens.setAlpha(0.5f);
+                    mBtnCoed.setAlpha(0.5f);
+                    mBtnNext.setAlpha(1f);
+                    mBtnNext.setEnabled(true);
+                }
+                else if(mtvDSkill.getText().toString().isEmpty()){
+                    mBtnMens.setAlpha(1f);
+                    mBtnWomens.setAlpha(1f);
+                    mBtnCoed.setAlpha(1f);
+                    mBtnNext.setAlpha(0.5f);
+                    mBtnNext.setEnabled(false);
+                }
+            }
+        });
+        mBtnWomens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mBtnWomens.getAlpha() != mBtnNext.getAlpha()) {
+                    mBtnMens.setAlpha(0.5f);
+                    mBtnWomens.setAlpha(1f);
+                    mBtnCoed.setAlpha(0.5f);
+                    mBtnNext.setAlpha(1f);
+                    mBtnNext.setEnabled(true);
+                }
+                else if(mtvDSkill.getText().toString().isEmpty()){
+                    mBtnMens.setAlpha(1f);
+                    mBtnWomens.setAlpha(1f);
+                    mBtnCoed.setAlpha(1f);
+                    mBtnNext.setAlpha(0.5f);
+                    mBtnNext.setEnabled(false);
+                }
+            }
+        });
+        mBtnCoed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mBtnCoed.getAlpha() != mBtnNext.getAlpha()) {
+                    mBtnMens.setAlpha(0.5f);
+                    mBtnWomens.setAlpha(0.5f);
+                    mBtnCoed.setAlpha(1f);
+                    mBtnNext.setAlpha(1f);
+                    mBtnNext.setEnabled(true);
+
+                }
+                else if(mtvDSkill.getText().toString().isEmpty()){
+                    mBtnMens.setAlpha(1f);
+                    mBtnWomens.setAlpha(1f);
+                    mBtnCoed.setAlpha(1f);
+                    mBtnNext.setAlpha(0.5f);
+                    mBtnNext.setEnabled(false);
+
+                }
+            }
+        });
+        mBtnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent skillIntent = new Intent(v.getContext(), SkillActivity.class);
+                startActivityForResult(skillIntent, REQ_CODE);
+            }
+        });
     }
 
-    private void findViews() {
-        Button mBtnNext = (Button) findViewById(R.id.btnNext);
+    private void findViews(){
+        mBtnMens = (Button) findViewById(R.id.mens);
+        mBtnWomens = (Button) findViewById(R.id.womens);
+        mBtnCoed = (Button) findViewById(R.id.co_ed);
+        mBtnNext= (Button) findViewById(R.id.btnBackNext);
+        mBtnNextBG = (Button) findViewById(R.id.next);
+        mtvDSkill = (TextView)findViewById(R.id.tvLeague);
+        mtvIAmText = (TextView)findViewById(R.id.tvSkill);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    public void mens(View view){
-        Button mBtnMens = (Button) findViewById(R.id.btnMens);
-        mBtnMens.getBackground().setAlpha(64);
-    }
-    public void womens(View view){
-        Button mBtnWomens = (Button) findViewById(R.id.btnWomens);
-        mBtnWomens.getBackground().setAlpha(64);
-    }
-    public void coed(View view){
-        Button mBtnCoed = (Button) findViewById(R.id.btnCoed);
-        mBtnCoed.getBackground().setAlpha(64);
-    }
-    public void next(View view){
-        Intent intent = new Intent(this, SkillActivity.class);
-        startActivity(intent);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQ_CODE) {
+                String replyMsg = data.getStringExtra(SkillActivity.PASS_STRING);
+                mtvIAmText.setVisibility(View.VISIBLE);
+                mtvDSkill.setVisibility(View.VISIBLE);
+                mtvDSkill.setText(replyMsg);
+                mBtnNext.setVisibility(View.GONE);
+                mBtnNextBG.setVisibility(View.GONE);
+                if(mBtnMens.getAlpha()!=1f)
+                    mBtnMens.setEnabled(false);
+                if(mBtnWomens.getAlpha()!=1f)
+                    mBtnWomens.setEnabled(false);
+                if(mBtnCoed.getAlpha()!=1f)
+                    mBtnCoed.setEnabled(false);
+            }
+        }
     }
 
 }
--- app/src/main/java/com/genson/amor/swoosh/SkillActivity.java
+++ app/src/main/java/com/genson/amor/swoosh/SkillActivity.java
@@ -1,28 +1,92 @@
 package com.genson.amor.swoosh;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-public class SkillActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class SkillActivity extends AppCompatActivity{
+
+    private Button mBtnBeginner;
+    private Button mBtnBaller;
+    private Button mBtnFinish;
+    public static final String PASS_STRING = "";
+
+    public SkillActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skill);
+        findViews();
+        mBtnBeginner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mBtnBeginner.getAlpha()!=mBtnFinish.getAlpha()) {
+                    mBtnBeginner.setAlpha(1f);
+                    mBtnBaller.setAlpha(0.5f);
+                    mBtnFinish.setAlpha(1f);
+                    mBtnFinish.setEnabled(true);
+                    mBtnFinish.setText("Finish");
+                }
+                else{
+                    mBtnBeginner.setAlpha(1f);
+                    mBtnBaller.setAlpha(1f);
+                    mBtnFinish.setAlpha(0.5f);
+                    mBtnFinish.setEnabled(false);
+                    mBtnFinish.setText("");
+                }
+            }
+        });
+        mBtnBaller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mBtnBaller.getAlpha()!=mBtnFinish.getAlpha()) {
+                    mBtnBeginner.setAlpha(0.5f);
+                    mBtnBaller.setAlpha(1f);
+                    mBtnFinish.setAlpha(1f);
+                    mBtnFinish.setEnabled(true);
+                    mBtnFinish.setText("Finish");
+                }
+                else{
+                    mBtnBeginner.setAlpha(1f);
+                    mBtnBaller.setAlpha(1f);
+                    mBtnFinish.setAlpha(0.5f);
+                    mBtnFinish.setEnabled(false);
+                    mBtnFinish.setText("");
+                }
+            }
+        });
+        mBtnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Iam = "";
+                if(mBtnBeginner.getAlpha()==1f) {
+                    Iam = mBtnBeginner.getText().toString();
+                }
+                else if(mBtnBaller.getAlpha()==1f) {
+                    Iam = mBtnBaller.getText().toString();
+                }
+                Intent data = new Intent();
+                data.putExtra(PASS_STRING, Iam);
+                setResult(Activity.RESULT_OK, data);
+                finish();
+
+
+            }
+        });
     }
 
-    public void btnFinishActivation(View view){
-        EditText mEtFinish = (EditText) findViewById(R.id.etFinish);
-        Button btnFinish = (Button) findViewById(R.id.btnFinish);
-        btnFinish.setOnClickListener(this);
+    private void findViews()
+    {
+        mBtnFinish = (Button) findViewById(R.id.finish);
+        mBtnBeginner = (Button) findViewById(R.id.beginner);
+        mBtnBaller = (Button) findViewById(R.id.baller);
     }
 
-    @Override
-    public void onClick(View v) {
-        Toast toast = Toast.makeText(this, "Finish", Toast.LENGTH_SHORT);
-    }
+
 }
